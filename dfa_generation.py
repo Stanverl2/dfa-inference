b9a5acf581daee8e98d917e9b076303e4a26ca06
Stan Verlaan <s.verlaan@student.tudelft.nl>
2025-05-19 12:31:41 +0200

    Changed some typos and consistency issues in variable names

--- dfa_generation.py
+++ dfa_generation.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 
-from dfa import Node
+from dfa import Node, render_dfa
 
 
 # Step 2 of the algorithm , creating geometric distribution.
@@ -30,11 +30,11 @@ def add_undefined_edge(fr: Node, to: Node):
         fr.a = to
 
 
-def isFullySaturated(node:Node) :
-    return len(unused_symols(node)) == 0
+def is_fully_saturated(node: Node):
+    return len(unused_symbols(node)) == 0
 
 
-def unused_symols(node: Node):
+def unused_symbols(node: Node):
     res = []
     if node.a is None:
         res.append('a')
@@ -64,49 +64,49 @@ def get_incoming(target: Node, nodes):
 # s: self loop probability. s = 0.2
 # SOURCES: https://www.cs.le.ac.uk/people/nw91/Files/stamina_journal.pdf, https://dl.acm.org/doi/10.1145/1217299.1217301
 # returns Nodes. Nodes id = 0 is the start node.
-def generateDFA(n: int, a=None, f=0.31, b=0.385, s=0.2):
+def generate_dfa(n: int, a=None, f=0.31, b=0.385, s=0.2) -> list[Node]:
     nodes = []
-    globaly_visited = set()
+    globally_visited = set()
 
-    def appendNode(nodeV: Node, locally_visited: set()):
+    def append_node(node_v: Node, locally_visited: set):
         # Step 1: Choose random ambassador and create an edge.
-        if nodeV not in nodes:
-            nodes.append(nodeV)
-        locally_visited.add(nodeV)
+        if node_v not in nodes:
+            nodes.append(node_v)
+        locally_visited.add(node_v)
 
-        candidates = [node for node in nodes if node.id != nodeV.id and node not in locally_visited]
+        candidates = [node for node in nodes if node.id != node_v.id and node not in locally_visited]
         if not candidates:
             return
 
         ambassador = random.choice(candidates)
         locally_visited.add(ambassador)
-        available_symbols = unused_symols(ambassador)
+        available_symbols = unused_symbols(ambassador)
         if not available_symbols:
             locally_visited.add(ambassador)
-            appendNode(nodeV, locally_visited)
-            globaly_visited.add(ambassador)
+            append_node(node_v, locally_visited)
+            globally_visited.add(ambassador)
             # Everytime a new state is considered, there should be something pointing towards it. If not, try again.
             return
 
         chosen_symbol = random.choice(available_symbols)
-        add_edge(ambassador, nodeV, chosen_symbol)
+        add_edge(ambassador, node_v, chosen_symbol)
 
         # Add self loops
         if random.random() < s:
-            available_symbols = unused_symols(ambassador)
+            available_symbols = unused_symbols(ambassador)
             if available_symbols:
                 chosen_symbol = random.choice(available_symbols)
                 add_edge(ambassador, ambassador, chosen_symbol)
-            available_symbols = unused_symols(ambassador)
+            available_symbols = unused_symbols(ambassador)
             if len(available_symbols) == 0:
-                globaly_visited.add(ambassador)
+                globally_visited.add(ambassador)
 
         # Step 2: generate x,y
         x = geometric(f / (1 - f))
         y = geometric(f*b / (1 - f*b))
 
         # Step 3
-        visited = locally_visited | globaly_visited
+        visited = locally_visited | globally_visited
         unvisited_out = [n for n in get_neighbours(ambassador) if n not in visited]
         unvisited_in = [n for n in get_incoming(ambassador, nodes) if n not in visited]
 
@@ -116,43 +116,31 @@ def generateDFA(n: int, a=None, f=0.31, b=0.385, s=0.2):
 
         for neigh in all_neigh:
             locally_visited.add(neigh)          # local nodes cannot be revisited
-            globaly_visited.add(ambassador)     # add to global, cause otherwise we have separate components
-            add_undefined_edge(nodeV, neigh)
-            appendNode(neigh, locally_visited)
+            globally_visited.add(ambassador)     # add to global, cause otherwise we have separate components
+            add_undefined_edge(node_v, neigh)
+            append_node(neigh, locally_visited)
 
     # We start building our DFA from here. Start node is i=0.
     for i in range(n):
-        newNode = Node(i, state=random.random() < 0.5)
-        appendNode(newNode, set())
+        new_node = Node(i, state=random.random() < 0.5)
+        append_node(new_node, set())
 
     # Fill in missing edges.
     for node in nodes:
-        unused = unused_symols(node)
+        unused = unused_symbols(node)
         for sym in unused:
             target = random.choice(nodes)
             add_edge(node, target, sym)
 
     return nodes
 
+
 # Example:
-nodes = generateDFA(4, ())
+nodes = generate_dfa(4, ())
 for node in nodes:
     print("Current node:")
+    if node.id == 0:
+        render_dfa(node)
     print(node.id)
     print(node.a.id)
     print(node.b.id)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
